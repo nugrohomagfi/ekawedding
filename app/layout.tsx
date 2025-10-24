bdb015019f0dd6fc982c70f7409c9605933cc87a
sijawara <sijawaracoid@gmail.com>
2025-10-24 22:11:32 +0700

    Change default theme from 'system' to 'light'

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body className={geistSans.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
